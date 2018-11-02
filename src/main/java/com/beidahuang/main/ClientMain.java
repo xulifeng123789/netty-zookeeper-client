66a460a825bd1962ba3e97af2de3f563db8f4cfd
xulifeng <1051068352@qq.com>
2018-11-02 14:18:47 +0800

    新增通过spring配置文件生成代理对象调用远程服务

--- src/main/java/com/beidahuang/main/ClientMain.java
+++ src/main/java/com/beidahuang/main/ClientMain.java
@@ -4,6 +4,7 @@ import com.beidahuang.discovery.IDiscovery;
 import com.beidahuang.discovery.impl.DiscoveryImpl;
 import com.beidahuang.handler.RpcClientHandler;
 import com.beidahuang.hello.Hello;
+import com.beidahuang.idcard.IdCard;
 import com.beidahuang.rpcrequest.RpcRequest;
 import com.beidahuang.user.User;
 import io.netty.bootstrap.Bootstrap;
@@ -20,6 +21,7 @@ import io.netty.handler.codec.serialization.ClassResolvers;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 import io.netty.handler.codec.serialization.ObjectEncoder;
 import org.apache.zookeeper.KeeperException;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
@@ -31,46 +33,54 @@ public class ClientMain {
 
     public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
 
-        IDiscovery discovery = new DiscoveryImpl();
-        final String discoveryUrl = discovery.discovery("com.beidahuang.hello.Hello");
+//        IDiscovery discovery = new DiscoveryImpl();
+//        final String discoveryUrl = discovery.discovery("com.beidahuang.hello.Hello");
 
-        //创建访问接口的代理
-        User proxy = (User) Proxy.newProxyInstance(User.class.getClassLoader(), new Class[]{User.class}, new InvocationHandler() {
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                //封装RpcRequest对象
-                RpcRequest rpcRequest = new RpcRequest();
-                rpcRequest.setMethodName(method.getName());
-                rpcRequest.setClassName(method.getDeclaringClass().getName());
-                rpcRequest.setMethodTypeParam(method.getParameterTypes());
-                rpcRequest.setParams(args);
-                String[] split = discoveryUrl.split(":");
-                String ip = split[0];
-                String port = split[1];
-                final RpcClientHandler rpcClientHandler = new RpcClientHandler();
-                //与服务端通信
-                EventLoopGroup group = new NioEventLoopGroup();
-                Bootstrap bootstrap = new Bootstrap();
-                bootstrap.group(group);
-                bootstrap.channel(NioSocketChannel.class);
-                bootstrap.handler(new ChannelInitializer<SocketChannel>() {
-                    protected void initChannel(SocketChannel ch) throws Exception {
 
-                        ChannelPipeline channelPipeline = ch.pipeline();
-                        channelPipeline.addLast("encoder",new ObjectEncoder());
-                        channelPipeline.addLast("decoder",new ObjectDecoder(Integer.MAX_VALUE, ClassResolvers.cacheDisabled(this.getClass().getClassLoader())));
-                        channelPipeline.addLast(rpcClientHandler);
-                    }
-                });
-
-                ChannelFuture f = bootstrap.connect(ip,Integer.parseInt(port)).sync();
-                f.channel().writeAndFlush(rpcRequest);//写数据
-                f.channel().closeFuture().sync();
-                return rpcClientHandler.getResponse();
-            }
-        });
-
-       List<String> resultList =  proxy.findUserNameById(1L);
-        System.out.println(resultList.size());
+        ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(new String[]{"META-INF/dubbo-consumer.xml"});
+        Hello hello =(Hello) classPathXmlApplicationContext.getBean("hello");
+        System.out.println(hello.sayHello("yanjie"));
+        User user = (User)classPathXmlApplicationContext.getBean("user");
+        System.out.println(user.findUserNameById(1L));
+        IdCard idCard = (IdCard) classPathXmlApplicationContext.getBean("idCard");
+        System.out.println(idCard.findIdCardById(1L));
+//        //创建访问接口的代理
+//        User proxy = (User) Proxy.newProxyInstance(User.class.getClassLoader(), new Class[]{User.class}, new InvocationHandler() {
+//            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                //封装RpcRequest对象
+//                RpcRequest rpcRequest = new RpcRequest();
+//                rpcRequest.setMethodName(method.getName());
+//                rpcRequest.setClassName(method.getDeclaringClass().getName());
+//                rpcRequest.setMethodTypeParam(method.getParameterTypes());
+//                rpcRequest.setParams(args);
+//                String[] split = discoveryUrl.split(":");
+//                String ip = split[0];
+//                String port = split[1];
+//                final RpcClientHandler rpcClientHandler = new RpcClientHandler();
+//                //与服务端通信
+//                EventLoopGroup group = new NioEventLoopGroup();
+//                Bootstrap bootstrap = new Bootstrap();
+//                bootstrap.group(group);
+//                bootstrap.channel(NioSocketChannel.class);
+//                bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+//                    protected void initChannel(SocketChannel ch) throws Exception {
+//
+//                        ChannelPipeline channelPipeline = ch.pipeline();
+//                        channelPipeline.addLast("encoder",new ObjectEncoder());
+//                        channelPipeline.addLast("decoder",new ObjectDecoder(Integer.MAX_VALUE, ClassResolvers.cacheDisabled(this.getClass().getClassLoader())));
+//                        channelPipeline.addLast(rpcClientHandler);
+//                    }
+//                });
+//
+//                ChannelFuture f = bootstrap.connect(ip,Integer.parseInt(port)).sync();
+//                f.channel().writeAndFlush(rpcRequest);//写数据
+//                f.channel().closeFuture().sync();
+//                return rpcClientHandler.getResponse();
+//            }
+//        });
+//
+//       List<String> resultList =  proxy.findUserNameById(1L);
+//        System.out.println(resultList.size());
 
     }
 }
